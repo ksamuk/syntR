cae352b7ccb247457c121fe1be8cd0b06789c46f
Kieran Samuk <ksamuk@Kierans-MacBook-Pro.local>
2018-10-12 13:09:09 -0400

    adding data

--- setup_package.R
+++ setup_package.R
@@ -4,6 +4,14 @@ library("devtools")
 library("usethis")
 library("roxygen2")
 
+# git/github
+use_git()
+# set up a new github personal access token
+# after the 'cat', restart Rstudio
+browse_github_pat() # interactive
+edit_r_environ() # interactive
+use_github()
+
 # dependencies
 use_package("dplyr")
 use_package("tidyr")
@@ -16,20 +24,11 @@ use_package("viridisLite")
 ann_pet_map <- read.table("data/ann_pet_sample_comparison.txt", h = T)
 ann_chr_length <- read.table("data/Ann_chr_max.txt", h = F) # needs chrosome names?
 use_data(ann_pet_map)
+use_data(ann_chr_length)
 #use_data(ann_chr_length)
 
 # documentation
 document()
-
-# git/github
-
-use_git()
-# set up a new github personal access token
-# after the 'cat', restart Rstudio
-browse_github_pat()
-edit_r_environ()
-#cat("GITHUB_PAT=[insert PAT here]\n", file = file.path(normalizePath("~/"), ".Renviron"), append = TRUE)
-use_github()
 
 # load package
 load_all()
