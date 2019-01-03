e08841bc330a74c96610a6c6acb5baf8a67fb018
ksamuk <ksamuk@gmail.com>
2019-01-03 14:46:13 -0500

    fixing borked logo

--- R/create_sticker.R
+++ R/create_sticker.R
@@ -13,7 +13,7 @@ df <- data.frame(x, y, clust_color)
 
 sub_plot <- df %>%
   ggplot(aes(x = x, y = y, color = clust_color))+
-  geom_point(size = 2.5, alpha = 1.0, shape = 16) +
+  geom_point(size = 2.35, alpha = 1.0, shape = 16) +
   theme_classic()+
   ylab("") + xlab("") +
   scale_color_brewer(type = "qual", palette = "Set1")+
@@ -23,8 +23,8 @@ sub_plot <- df %>%
         legend.position = "none")
 
 sticker(sub_plot,
-        package = "syntR", p_size = 30, p_x = 1, p_y = 1.5,
-        s_x = 0.9, s_y = 0.7, s_width = 1.1, s_height = 1.1,
+        package = "syntR", p_size = 10, p_x = 1, p_y = 1.5,
+        s_x = 0.9, s_y = 0.7, s_width = 1.2, s_height = 1.2,
         spotlight = FALSE, l_x = 1.0, l_y = 1.0, l_width = 5, l_height = 5, l_alpha = 0.8,
         h_fill = "white", h_color = "black",
         #h_fill = "#3366CC", h_color = "#003399",
